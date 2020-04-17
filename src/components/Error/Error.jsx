7610175df4dfa66b345b1578a5c9adea506724a0
Shawn Yates <yates.shawnp@gmail.com>
2020-04-16 17:16:41 -0700

    responsive menu, some style cleanup

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { CENTER_ELEMENT_MIXIN } from '../../styles/mixins';
+
 const StyledContainer = styled.div`
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 20px;
+  font-size: 28px;
+
+  ${CENTER_ELEMENT_MIXIN}
 `;
 
 function Error() {
   return (
     <StyledContainer>
       <span role="img" aria-label="Crying Emoji">😭</span>
-      Oh no! Something went wrong. Try refreshing the page to see if that helps.
-      <span role="img" aria-label="Crossed Fingers Emoji">🤞</span>
+      Oh no! Something went wrong.
+      <span role="img" aria-label="Crying Emoji">😭</span>
     </StyledContainer>
   );
 }
---
+++ src/styles/mixins.js
@@ -0,0 +1,15 @@
+export const TABLE_POSITION_MIXIN = `
+  position: absolute;
+  top: 100px;
+  left: 250px;
+
+  @media only screen and (max-width: 1040px) {
+    left: 10%;
+  }
+`;
+
+export const CENTER_ELEMENT_MIXIN = `
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
